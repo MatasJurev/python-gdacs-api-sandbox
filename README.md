0da384d077e5a93340e7f248884ab26e30304738
Matas <matukas.jurevicius@gmail.com>
2022-02-25 19:25:20 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# python gdacs weather report api example
- 
+# GDACS api python example
+
+Api used to get information about natural disasters from all around the world
+
+I've made an example of this api during my free time.
+Tested on python 3.10
+
+More on gdacs api:
+
+https://pypi.org/project/gdacs-api/
+https://github.com/Kamparia/gdacs-api
+
+gdacs website:
+
+https://gdacs.org/
+
+I may improve this in the future
+
+Feel free to fork this repository or contribute
---
+++ main.py
@@ -0,0 +1,54 @@
+from gdacs.api import GDACSAPIReader
+
+import json
+from os import system, name
+from time import sleep as amimir
+
+
+client = GDACSAPIReader()
+d={}
+c={}
+
+
+#clear text from console window, depending on os type
+def clear_console():
+    system('cls' if name=='nt' else 'clear')
+
+
+#function, responsible for calling other methods for data and displaying it
+def display_data():
+    events = client.latest_events()
+    for entry in sorted(parse_data(str(event)) for event in events):
+        print(f'{entry}\n')
+    print('Data is updated every 5 minutes.')
+
+
+#python data parser using json (bad solution)
+def parse_data(data : str) -> str:
+    global c
+    global d
+    try:
+        d = json.loads(data.replace('\'', '"').replace('None', '"None"'))
+        c = d["geo:Point"]
+        b = d["gdacs:population"]
+        return f'{d["gdacs:country"]} | {d["gdacs:fromdate"]} | {d["title"]} | {d["description"]} | {d["link"]} | coordinates: {c["geo:lat"]}, {c["geo:long"]} | affection: {b["#text"]}'
+
+    except:
+        return f'{d["gdacs:country"]} | {d["gdacs:fromdate"]} | {d["title"]} | {d["description"]} | {d["link"]} | coordinates: {c["geo:lat"]}, {c["geo:long"]}'
+        
+
+def main():
+    try:
+        while 1:
+            clear_console()
+            display_data()
+            amimir(300)
+            
+    except Exception as e:
+        print(f'Error: {e} - will retry in 5 minutes.')
+        amimir(300)
+        main()
+
+
+if __name__ == '__main__':
+    main()
